4abbb478292185c2317d90c679bea53df6895248
Fowzi Shakkour <fowzis@gmail.com>
2018-09-03 22:40:15 +0300

    Updated Version

--- QuadTree/Art.cs
+++ QuadTree/Art.cs
@@ -7,11 +7,15 @@ namespace QuadTree
     static class Art
     {
         public static Texture2D Pixel { get; private set; }		// a single white pixel
+        public static Texture2D Dot16 { get; private set; }		// a single white pixel
+        public static Texture2D Dot32 { get; private set; }		// a single white pixel
 
         public static void Load(ContentManager content)
         {
             // TODO
-            Pixel = content.Load<Texture2D>(@"Images\Pixel");
+            Pixel = content.Load<Texture2D>(@"Pixel");
+            Dot16 = content.Load<Texture2D>(@"Dot16x16");
+            Dot32 = content.Load<Texture2D>(@"Dot32x32");
         }
     }
 }
